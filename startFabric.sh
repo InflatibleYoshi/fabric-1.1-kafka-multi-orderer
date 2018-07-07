83a49a9a9e26ac61cc66843bb7375f55a61641cc
InflatibleYoshi <dananth@protonmail.com>
2018-07-07 15:38:28 +0000

    11 change to startFabric

--- startFabric.sh
+++ startFabric.sh
@@ -35,6 +35,8 @@ sleep ${FABRIC_START_TIMEOUT}
 # Create the channel
 docker exec peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c composerchannel -f /etc/hyperledger/configtx/composer-channel.tx --tls --cafile /etc/hyperledger/msp/orderer/msp/tlscacerts/tlsca.example.com-cert.pem
 
+sleep ${FABRIC_START_TIMEOUT}
+
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b composerchannel.block --tls --cafile /etc/hyperledger/msp/orderer/msp/tlscacerts/tlsca.example.com-cert.pem
 
