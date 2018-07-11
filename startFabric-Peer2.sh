29857a9326589e51a7c8b9e0c4a5a91289617d31
Daniel Ananth <dananth@protonmail.com>
2018-07-11 02:30:59 +0000

    20 multi-orderer working will try composer tomorrow

--- startFabric-Peer2.sh
+++ startFabric-Peer2.sh
@@ -31,6 +31,7 @@ ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-peer2.yml up -d
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
 echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
 
 docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer2.org1.example.com peer channel fetch config -o orderer0.example.com:7050 -c composerchannel --tls --cafile /etc/hyperledger/msp/orderer/msp/tlscacerts/tlsca.example.com-cert.pem
 # docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.example.com/msp" peer0.org2.example.com peer channel fetch config -o orderer.example.com:7050 -c composerchannel
