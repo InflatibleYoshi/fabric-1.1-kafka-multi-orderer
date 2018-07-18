523817601a2a7fc148e9993c1911d1b5dd964a72
Daniel Ananth <lca_daniel_a@yahoo.com>
2018-07-18 02:56:35 -0400

    A8 i will sleep now

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,6 +40,7 @@ resource "aws_instance" "fabric" {
     inline = [
       "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 192.168.1.240:/ /home/ubuntu/fabric-1.1-kafka-multi-orderer",
       "sudo git clone https://github.com/InflatibleYoshi/fabric-1.1-kafka-multi-orderer",
+      "chmod -R 777 fabric-1.1-kafka-multi-orderer",
       "cd fabric-1.1-kafka-multi-orderer",
       "sudo python start.py ${var.n} terraform",
       "cd composer",
