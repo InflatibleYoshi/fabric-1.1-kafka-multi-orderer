24bc0c597043d7f79a482e2c9ce7e0525277afe6
Daniel Ananth <lca_daniel_a@yahoo.com>
2018-07-24 22:31:22 -0400

    A31 Delay timings : zookeeper -> Kafka -> orderers

--- config-all/docker-compose-peer-n.py
+++ config-all/docker-compose-peer-n.py
@@ -36,11 +36,6 @@ services:
         - ./:/etc/hyperledger/configtx
         - ./crypto-config/ordererOrganizations/example.com/orderers/orderer""" + str(i + 1) + """.example.com/msp:/etc/hyperledger/msp/orderer/msp
         - ./crypto-config/ordererOrganizations/example.com/orderers/orderer""" + str(i + 1) + """.example.com/tls:/etc/hyperledger/tls/orderer/tls
-    depends_on:
-      - kafka""" + str(4*i + 4) + """
-      - kafka""" + str(4*i + 5) + """
-      - kafka""" + str(4*i + 6) + """
-      - kafka""" + str(4*i + 7) + """
 
   peer""" + str(2 + (i*2)) + """.org1.example.com:
     container_name: peer""" + str(2 + (i*2)) + """.org1.example.com
@@ -206,4 +201,78 @@ services:
     text_file.write(file)
     text_file.close()
 
+    kafka = """version: '2'
+
+services:
+  kafka""" + str(4*i + 4) + """:
+    container_name: kafka""" + str(4*i + 4) + """
+    # image: wurstmeister/kafka:latest
+    image: hyperledger/fabric-kafka
+    restart: always
+    environment:
+      - KAFKA_MESSAGE_MAX_BYTES=103809024
+      - KAFKA_REPLICA_FETCH_MAX_BYTES=103809024 
+      - KAFKA_UNCLEAN_LEADER_ELECTION_ENABLE=false
+      - KAFKA_BROKER_ID=""" + str(4*i + 4) + """
+      - KAFKA_MIN_INSYNC_REPLICAS=2
+      - KAFKA_DEFAULT_REPLICATION_FACTOR=3
+      - KAFKA_LOG_RETENTION_MS=-1
+      - KAFKA_ZOOKEEPER_CONNECT={IP-HOST-1}:2181,{IP-HOST-1}:3181,{IP-HOST-1}:4181
+    ports:
+      - 9092:9092
+
+  kafka""" + str(4*i + 5) + """:
+    container_name: kafka""" + str(4*i + 5) + """
+    # image: wurstmeister/kafka:latest
+    image: hyperledger/fabric-kafka
+    restart: always
+    environment:
+      - KAFKA_MESSAGE_MAX_BYTES=103809024
+      - KAFKA_REPLICA_FETCH_MAX_BYTES=103809024
+      - KAFKA_UNCLEAN_LEADER_ELECTION_ENABLE=false
+      - KAFKA_BROKER_ID=""" + str(4*i + 5) + """
+      - KAFKA_MIN_INSYNC_REPLICAS=2
+      - KAFKA_DEFAULT_REPLICATION_FACTOR=3
+      - KAFKA_LOG_RETENTION_MS=-1
+      - KAFKA_ZOOKEEPER_CONNECT={IP-HOST-1}:2181,{IP-HOST-1}:3181,{IP-HOST-1}:4181
+    ports:
+      - 10092:9092
+
+  kafka""" + str(4*i + 6) + """:
+    container_name: kafka""" + str(4*i + 6) + """
+    # image: wurstmeister/kafka:latest
+    image: hyperledger/fabric-kafka
+    restart: always
+    environment:
+      - KAFKA_MESSAGE_MAX_BYTES=103809024
+      - KAFKA_REPLICA_FETCH_MAX_BYTES=103809024
+      - KAFKA_UNCLEAN_LEADER_ELECTION_ENABLE=false
+      - KAFKA_BROKER_ID=""" + str(4*i + 6) + """
+      - KAFKA_MIN_INSYNC_REPLICAS=2
+      - KAFKA_DEFAULT_REPLICATION_FACTOR=3
+      - KAFKA_LOG_RETENTION_MS=-1
+      - KAFKA_ZOOKEEPER_CONNECT={IP-HOST-1}:2181,{IP-HOST-1}:3181,{IP-HOST-1}:4181
+    ports:
+      - 11092:9092
+
+  kafka""" + str(4*i + 7) + """:
+    container_name: kafka""" + str(4*i + 7) + """
+    # image: wurstmeister/kafka:latest
+    image: hyperledger/fabric-kafka
+    restart: always
+    environment:
+      - KAFKA_MESSAGE_MAX_BYTES=103809024
+      - KAFKA_REPLICA_FETCH_MAX_BYTES=103809024
+      - KAFKA_UNCLEAN_LEADER_ELECTION_ENABLE=false
+      - KAFKA_BROKER_ID=""" + str(4*i + 7) + """
+      - KAFKA_MIN_INSYNC_REPLICAS=2
+      - KAFKA_DEFAULT_REPLICATION_FACTOR=3
+      - KAFKA_LOG_RETENTION_MS=-1
+      - KAFKA_ZOOKEEPER_CONNECT={IP-HOST-1}:2181,{IP-HOST-1}:3181,{IP-HOST-1}:4181
+    ports:
+      - 12092:9092
+    """
+    text_file = open("composer/docker-compose-kafka-peer" + str(i + 2) + ".yml", "w")
+    text_file.write(kafka)
+    text_file.close()
 
--- config-all/startFabric-peer-n.py
+++ config-all/startFabric-peer-n.py
@@ -52,3 +52,37 @@ docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.ex
     text_file = open("startFabric-Peer" + str(i + 2) + ".sh", "w")
     text_file.write(file)
     text_file.close()
+
+    file = """#!/bin/bash
+
+if [ -z ${FABRIC_START_TIMEOUT+x} ]; then
+ echo "FABRIC_START_TIMEOUT is unset, assuming 15 (seconds)"
+ export FABRIC_START_TIMEOUT=15
+else
+
+   re='^[0-9]+$'
+   if ! [[ $FABRIC_START_TIMEOUT =~ $re ]] ; then
+      echo "FABRIC_START_TIMEOUT: Not a number" >&2; exit 1
+   fi
+
+ echo "FABRIC_START_TIMEOUT is set to '$FABRIC_START_TIMEOUT'"
+fi
+
+# Exit on first error, print all commands.
+set -ev
+
+#Detect architecture
+ARCH=`uname -m`
+
+# Grab the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#
+
+# ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-kafka-peer""" + str(i + 2) + """.yml down
+ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-kafka-peer""" + str(i + 2) + """.yml up -d
+"""
+
+    text_file = open("startKafka-Peer" + str(i + 2) + ".sh", "w")
+    text_file.write(file)
+    text_file.close()
